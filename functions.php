dba31da9c8ca21a0b503186ced5f2abcefc73748
Christopher Crouch <chrisdc@gmail.com>
2014-05-24 17:30:10 +0100

    Registered widget areas for front page template

--- functions.php
+++ functions.php
@@ -78,6 +78,33 @@ function elucidate_widgets_init() {
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
+	
+	register_sidebar( array(
+		'name'          => __( 'Front Page Widget 1', 'elucidate' ),
+		'id'            => 'front-page-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+	
+	register_sidebar( array(
+		'name'          => __( 'Front Page Widget 2', 'elucidate' ),
+		'id'            => 'front-page-2',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+	
+	register_sidebar( array(
+		'name'          => __( 'Front Page Widget 3', 'elucidate' ),
+		'id'            => 'front-page-3',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 }
 add_action( 'widgets_init', 'elucidate_widgets_init' );
 
