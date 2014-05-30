445b388f00ee54cd93a1198ec1b9befbc44e6c19
Christopher Crouch <chrisdc@gmail.com>
2014-05-30 21:07:34 +0100

    Check is_single before adding link on header

--- content-image.php
+++ content-image.php
@@ -13,7 +13,11 @@
 		<?php } ?>
 		<?php elucidate_time(); ?>
 		
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<?php if ( is_single() ) { ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php } else { ?>
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<?php } ?>
 
 		<?php if ( get_the_author_meta( 'display_name' ) && is_multi_author() ) { ?>
 			<div class="byline">
